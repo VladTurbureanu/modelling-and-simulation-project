2fb8fe488dcadc210f0d5ccad189ed087d121928
Vlad Turbureanu <vladturbureanu@gmail.com>
2021-09-30 14:43:19 +0200

    Updated project.m
    Update

--- project.m
+++ project.m
@@ -1,17 +1,31 @@
 
-n = 20; % number of nodes in the graph
-density = 0.2; % a rough estimate of the amount of edges       
-A = sprand( n, n, density ); % generate adjacency matrix at random
-% normalize weights to sum to num of edges
-A = tril( A, -1 );    
-A = spfun( @(x) x./nnz(A), A );    
-% make it symmetric (for undirected graph)
-%A = A + A.';
+% n = 20; % number of nodes in the graph
+% density = 0.2; % a rough estimate of the amount of edges       
+% A = sprand( n, n, density ); % generate adjacency matrix at random
+% % normalize weights to sum to num of edges
+% A = tril( A, -1 );    
+% A = spfun( @(x) x./nnz(A), A );    
+
+N = 20;
+s = floor(rand(1, N-1) .* (1:N-1)) + 1;
+t = 2:N;
+
 
 nodevalues = randi([0 1],20,1);
 nodenames = string((1:20)');
-%T = table(rowTimes,X,Y,'VariableNames',{'Latitude','Longitude'});
 NodeTable = table(nodenames,nodevalues,'VariableNames',{'Name','NodeValues'});
+G = digraph(s,t);
 
-G = digraph(A,NodeTable);
 plot(G,'NodeLabel',nodevalues);
+
+
+
+
+
+
+
+
+
+
+
+
