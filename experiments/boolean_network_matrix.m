7561908e7fe5f1f7a5056b328ae81028f82e3ab2
LdeVree <l.de.vree@student.rug.nl>
2021-10-15 16:05:46 +0200

    Restructuring + bulk experiments with cycles

--- experiments/boolean_network_matrix.m
+++ experiments/boolean_network_matrix.m
@@ -1,23 +1,21 @@
-function main()
-    [matrix_size, no_of_connections, no_of_iterations] = read_input();
+function cycle_length = boolean_network_matrix(matrix_size, no_of_connections, no_of_iterations, visualization, run_to_end)
+    %[matrix_size, no_of_connections, no_of_iterations] = read_input();
     
     % Each node in the network has a boolean variable attached to it (0 or 1)
     % Increase the number after '>' for less ones, decrease it for more.
-    A = rand(matrix_size,matrix_size) > 0.7;
+    % We want an equal probability for zeroes and ones
+    A = rand(matrix_size,matrix_size) > 0.5;
 
     neighbouring_nodes = {};
+    truth_table_values = {};
     all_matrices = {};
     no_of_genes = matrix_size * matrix_size;
 
-    % Specific rules for transitioning
-    % Permutations of random values: no_of_inputs^no_of_connections
-    % e.g. random values with 3 input (-> 8 permutations)
-    % This should probably be outside of the loop 
-    truth_table_values = randi([0 1],1, 2^no_of_connections);
     % standard adjacency matrix of the network
     adjacency_matrix = zeros(no_of_genes,no_of_genes);
     
     cycle_cntr = 0;
+    cycle_length = -1;
 
     % state matrix will be used to count cycles in the states of the
     % network
@@ -26,8 +24,8 @@ function main()
         all_matrices{iteration} = A;
         % Each node in the network is connected to 3 other nodes (randomly)
         % Here we have 16 genes (in case of a 4x4 matrix), so 16 positions. 
-        % Q: can they be connected to itself ? For now, we assume yes because it's
-        % simpler
+        % Q: can they be connected to itself ? For now, we assume no -- see
+        % the while loop below
         neighbours_matrix = {};
         for gene = 1:no_of_genes
             % For each node, give 3 random values between 1 and
@@ -36,12 +34,21 @@ function main()
             % same)
             % So K = 3 in this case
             if iteration == 1
-                neighbouring_nodes{gene} = randperm(no_of_genes,no_of_connections);
+                neighbouring_nodes{gene} = randperm(no_of_genes, no_of_connections);
+                while ismember(gene, neighbouring_nodes{gene})
+                    neighbouring_nodes{gene} = randperm(no_of_genes, no_of_connections);
+                end
                 % create the adjacency matrix by using the neighboring
                 % nodes
                 for i = 1:length(neighbouring_nodes{gene})
                     adjacency_matrix(gene,neighbouring_nodes{gene}(i))=1;
                 end
+                
+                % Specific rules for transitioning, specific per gene
+                % Permutations of random values: no_of_inputs^no_of_connections
+                % e.g. random values with 3 input (-> 8 permutations)
+                truth_table_values{gene} = randi([0 1],1, 2^no_of_connections);
+                
             end
             
             % Get the binary values of the neighbours
@@ -57,64 +64,38 @@ function main()
         % Synchronous update
         for gene_update = 1:no_of_genes
             neighbours = neighbours_matrix{gene_update};
-            new_matrix(gene_update) = truth_table(no_of_connections, neighbours, truth_table_values);
+            new_matrix(gene_update) = truth_table(no_of_connections, neighbours, truth_table_values{gene_update});
         end
-        disp("iteration " + iteration)
+        if mod(iteration, 100) == 0
+            disp("iteration " + iteration)
+        end
         A = new_matrix;
-        display_grid(A, iteration)
-        pause(0.3)
+        if visualization == 1
+            display_grid(A, iteration)
+            pause(0.3)
+        end
         
         [found_cycle, last_value] = find_cycle(A, all_matrices);
-        if found_cycle == 1
+        if run_to_end == 0 && found_cycle == 1
             disp('Found a cycle! In iteration and with the last value of finding this pattern:')
             iteration
-            last_value
             cycle_length = iteration - last_value
             cycle_cntr = cycle_cntr + 1;
-            if cycle_cntr == cycle_length
+            if cycle_cntr == 3
                 break;
             end
         end
     
     end
-    % display the network as a directed graph by using the adjacency matrix
-    figure;
-    G = digraph(adjacency_matrix);
-    plot(G);
     
-    
-end
-
-% Reads user input
-function [matrix_size, no_of_connections, no_of_iterations] = read_input()
-    prompt = "Please give a matrix size between 2 and 200 (e.g. 4-> 4x4): ";
-    matrix_size = input(prompt);
-    
-    % Ensuring correct input
-    while matrix_size < 2 || matrix_size > 200
-        msgbox("Invalid matrix size. Please choose a number between 2 and 200.");
-        matrix_size = input(prompt);
+    if visualization == 1
+        % display the network as a directed graph by using the adjacency matrix
+        figure;
+        G = digraph(adjacency_matrix);
+        plot(G);
     end
     
-    % Choose 2 or 3 as the other ones have not yet been properly implemented
-    prompt = "Please give the number of connections per gene (between 2 and 3) (for now, still need to implement more): ";
-    no_of_connections = input(prompt);
     
-    % Ensuring correct input
-    while no_of_connections < 1 || no_of_connections > 4
-        msgbox("Invalid number of connections. Please choose a number between 2 and 3. ");
-        no_of_connections = input(prompt);
-    end    
-
-    prompt = "Please give the number of iterations (between 1 and 1000): ";
-    no_of_iterations = input(prompt);
-    
-    % Ensuring correct input
-    while no_of_iterations < 1 || no_of_iterations > 1000
-        msgbox("Invalid number of iterations. Please choose a number between 1 and 1000. ");
-        no_of_connections = input(prompt);
-    end    
-
 end
 
 % It should be possible to do this more efficiently, will have a look at it later
@@ -153,9 +134,9 @@ function output = truth_table2(neighbours, truth_table_values)
     elseif neighbours == [0,1]
         output = truth_table_values(2);
     elseif neighbours == [1,0]
-        output = truth_table_values(2);
-    elseif neighbours == [1,1]
         output = truth_table_values(3);
+    elseif neighbours == [1,1]
+        output = truth_table_values(4);
     end
 end
 
---
+++ experiments/run_simulation.m
@@ -0,0 +1,50 @@
+function run_simulation()
+    [matrix_size, no_of_connections, no_of_iterations, visualization, run_to_end] = read_input();
+    
+    cycle_lengths = [];
+    for i = 1:100
+        disp("Found " + i + " / 100 cycles so far...")
+        cycle_lengths(i) = boolean_network_matrix(matrix_size, no_of_connections, no_of_iterations, visualization, run_to_end);
+    end
+    
+    cycle_lengths
+
+end
+
+% Reads user input
+function [matrix_size, no_of_connections, no_of_iterations, visualization, run_to_end] = read_input()
+    prompt = "Please give a matrix size between 2 and 200 (e.g. 4-> 4x4): ";
+    matrix_size = input(prompt);
+    
+    % Ensuring correct input
+    while matrix_size < 2 || matrix_size > 200
+        msgbox("Invalid matrix size. Please choose a number between 2 and 200.");
+        matrix_size = input(prompt);
+    end
+    
+    % Choose 2 or 3 as the other ones have not yet been properly implemented
+    prompt = "Please give the number of connections per gene (between 2 and 3) (for now, still need to implement more): ";
+    no_of_connections = input(prompt);
+    
+    % Ensuring correct input
+    while no_of_connections < 1 || no_of_connections > 4
+        msgbox("Invalid number of connections. Please choose a number between 2 and 3. ");
+        no_of_connections = input(prompt);
+    end    
+
+    prompt = "Please give the number of iterations (between 1 and 1000): ";
+    no_of_iterations = input(prompt);
+    
+    % Ensuring correct input
+    while no_of_iterations < 1 || no_of_iterations > 1000
+        msgbox("Invalid number of iterations. Please choose a number between 1 and 1000. ");
+        no_of_connections = input(prompt);
+    end    
+    
+    prompt = "Visualization: Type 1 for ON, 0 for OFF (without visualization makes it much faster): ";
+    visualization = input(prompt);
+    
+    prompt = "Run to end (1) or Find cycles (0): ";
+    run_to_end = input(prompt);
+
+end
